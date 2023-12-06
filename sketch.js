7e6f8afe8b11833f77c05da9dbc0ad685e189efb
dhyan-rai <7003944@spsd.sk.ca>
2023-12-06 12:17:14 -0600

    learned basics of p5play and experimented with p5play

--- sketch.js
+++ sketch.js
@@ -5,13 +5,23 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let player;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight, WEBGL);
+  // createCanvas(windowWidth, windowHeight);
+  new Canvas();
+  player = new Sprite();
+  player.color = "black";
 }
 
 function draw() {
   background(220);
+  clear();
 }
 
+function checkMovement() {
+  if (keyIsDown(87)) {
+    
+  }
+}
 
