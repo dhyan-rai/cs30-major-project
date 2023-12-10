37b632a96c4d9feaee6324b36a1f267779368acf
dhyan-rai <7003944@spsd.sk.ca>
2023-12-09 23:18:49 -0600

    tested sprite property sharing using p5party

--- testing/sketch.js
+++ testing/sketch.js
@@ -1,17 +1,19 @@
 //testing
 
 let guests, me;
-let myObj;
+let myObj, ground;
 let spriteArray = [];
-let sharedInfo;
+let shared;
+let guestNum;
 
+
+let ball;
 
 function preload() {
-  // partyConnect("wss:://demoserver.p5party.org", "testing-p5party-with-p5play");
   partyConnect("wss://demoserver.p5party.org", "the-test-party-DR");
 
-  sharedInfo = partyLoadShared("global", {visited: 0});
-  guests = partyLoadGuestShareds();
+  shared = partyLoadShared("global", {visited: 0, spriteArray: []});
+  // guests = partyLoadGuestShareds();
   me = partyLoadMyShared();
   guests = partyLoadGuestShareds();
 
@@ -19,22 +21,101 @@ function preload() {
 
 function setup() {
 
-  eval("partySetShared(me, {x: sharedInfo.visited * 100, y: sharedInfo.visited * 100, name: 'guest" + sharedInfo.visited + "'})");
+  eval("partySetShared(me, {x: (shared.visited+1) * 100, y: (shared.visited+1) * 100, name: 'guest" + shared.visited + "', properties: []})");
 
 
   // createCanvas(windowWidth, windowHeight);
   new Canvas(); 
 
+  // ball = new Sprite(200, 200, 100);
+  ground = new Sprite(0, height-100, 3000, 100, "static");
+  ground.rotation = 182;
+  world.gravity.y = 9.8;
+
+
+
+
   //p5party code
-  sharedInfo.visited++;
+
+  shared.visited++;
+  guestNum = shared.visited - 1;
 
   //p5play code
-  
-
+  eval("guest" + guestNum + " = new Sprite(0, 0, 50)");
+  // spriteArray.push(eval("guest" + guestNum));
+  shared.spriteArray.push("guest" + guestNum);
+  updateSelfProperties(eval("guest"+guestNum));
+  loadGuestSprites();  
 }
 
 function draw() {
+  
+  
   background(220);
+  clear();
+  updateSelfProperties(eval("guest"+guestNum));
+  updateGuestSprites();
 }
+
+
+// function updateSpriteProperties(obj) {
+//   let i = 0;
+//   for (let property of Object.values(obj)) {
+//     if(notFuncOrClass(property)) {
+//       me.properties[i] = property; 
+//       // testArr[i] = property
+//     }
+//     i++;
+//   }
+// }
+
+// function loadSprites() {
+//   for (let guest of guests) {
+//     eval(guest.name + " = new Sprite()");
+//     let i = 0;
+//     for (let property of guest.properties) {
+//       if (guest.properties[i] !== null) {
+//         eval(guest.name + "." + key + "= guest.properties[i]");
+//       }
+//       i++;
+
+//     }
+
+//   }
+// }
+
+function notFuncOrClass(value) {
+  return (
+    (typeof value !== 'function' && typeof value !== 'object') && typeof value !== null ||
+    (typeof value === 'object' && value.constructor === Object)
+  );
+}
+
+function updateSelfProperties(obj) {
+  me.properties[0] = obj.velocity.x;
+  me.properties[1] = obj.velocity.y;
+}
+
+function loadGuestSprites() {
+  for (let guest of guests) {
+    if (guest.name !== me.name) {
+      eval(guest.name + " = new Sprite()");
+      spriteArray.push(eval(guest.name));
+    }
+  }
+}
+
+function updateGuestSprites() {
+  for (let guest of guests) {
+    if (guest.name !== me.name) {
+      if (eval("typeof " + guest.name + "=== undefined")){
+        eval(guest.name + " = new Sprite(10,10,50)");
+      }
+      eval(guest.name + ".velocity.x = guest.properties[0]")
+      eval(guest.name + ".velocity.x = guest.properties[1]")
+    }
+  }
+}
+
 
 
