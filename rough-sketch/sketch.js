3672fed9348181a9da4f81bde7a8b3d53a6517b8
dhyan-rai <7003944@spsd.sk.ca>
2023-12-11 12:17:15 -0600

    tested colliders in p5play and worked on collider bug

--- rough-sketch/sketch.js
+++ rough-sketch/sketch.js
@@ -16,24 +16,9 @@ class Ball {
   }
 }
 
-
-
-// testObj = new Sprite(); 
-
-function preload() {
-  
-
-
-  //connecting to a party
-}
-
 function setup() {
   // createCanvas(windowWidth, windowHeight);
 
-  partyConnect(
-		"wss://demoserver.p5party.org", 
-		"p5party-integration-with-p5playssdfsdfsdfsdfsdfsdf"
-	);
 
 
   new Canvas();
@@ -41,49 +26,73 @@ function setup() {
   // ball.debug = true;
   ball.color = "black";
   ball.mass = 100;
+  ball.collider = "d";
+  ball.bounciness = 0;
+  // ball.debug = true;
+  ball.rotationLock = true;
+  ball.friction = 0;
+  ball.drag = 0;
 
-  obstacle = new Sprite(200, 200, 40, 40);
+  obstacle = new Sprite(400, 400, 40, 40, "k");
   obstacle.color = "green";
   obstacle.mass = 100;
   obstacle.rotationLock = true;
+  obstacle.bounciness = 0;
+  obstacle.friction = 0;
+  obstacle.drag = 0;
   
 }
 
 function draw() {
   background(220);
   clear();
+
   checkMovement();
+  retainPosition();
 }
 
 function checkMovement() {
-  if (keyIsPressed) {
+  if (keyIsDown) {
     if (kb.presses("w")) {
       ball.vel.y = -6;
+      ball.vel.x = 0;
+      // ball.move(20);
+      // ball.direction = -90;
     }
     if (kb.presses("s")) {
       ball.vel.y = 6;
+      ball.vel.x = 0;
+      // ball.direction = 90;
     }
     if (kb.presses("a")) {
       ball.vel.x = -6;
+      ball.vel.y = 0;
+      // ball.direction = 180;
     }
     if (kb.presses("d")) {
       ball.vel.x = 6;
+      ball.vel.y = 0;
+      // ball.direction = 0;
     }
   }
   else {
-    ball.vel.y = 0;
     ball.vel.x = 0;
+    ball.vel.y = 0;
   }
+
 }
 
 function drawBullet() {
   let x = mouseX - width / 2;
   let y = mouseY - height / 2;
   let a = atan2(y, x);
-  push();
   rotate(a);
 }
 
 
 
 
+function retainPosition() {
+  obstacle.vel.x = 0;
+  obstacle.vel.y = 0;
+}
